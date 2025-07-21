934bdedbfa1f24887af6343759ce474d9410ddbd
_AkshitMahajan <83849030+Akshit2004@users.noreply.github.com>
2025-07-21 17:12:36 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import LandingPage from './pages/LandingPage/LandingPage';
+
+function App() {
+  return (
+    <LandingPage />
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to Aaradhya Trust</h1>
+      <p>Your support makes a difference.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
